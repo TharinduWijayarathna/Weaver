a86db2c76aa8ff46db6a8e111eb6a9a8bacdd1e3
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-07-04 18:44:32 +0530

    Page Optimizations

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
+use App\Http\Controllers\Client\HomeController as WebsiteHomeController;
+use App\Http\Controllers\Admin\HomeController as DashboardHomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WebsiteHomeController::class, 'index'])->name('website.home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardHomeController::class, 'index'])->name('dashboard.home');
 });
